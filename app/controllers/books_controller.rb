01fef96a42e1dbf5f3523a65576641e5926e74be
Ubuntu <ubuntu@ip-172-31-28-47.ap-northeast-1.compute.internal>
2025-11-09 19:18:13 +0000

    update3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,7 +7,7 @@ class BooksController < ApplicationController
     def create
         book = Book.new(book_params)
         book.save
-        redirect_to("/top")
+        redirect_to book_path(book.id)
     end
 
     def index
@@ -15,12 +15,20 @@ class BooksController < ApplicationController
     end
 
     def show
+        @book = Book.find(params[:id])
     end
 
     def edit
+        @book = Book.find(params[:id])
     end
 
-    private
+    def update
+        book = Book.find(params[:id])
+        book.update(book_params)
+        redirect_to book_path(book.id)
+    end
+
+        private
 
     def book_params
         params.require(:book).permit(:title, :body)
