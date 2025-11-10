4b3aea07eef7bbe7717dd7e68fb4b38194e44a38
Ubuntu <ubuntu@ip-172-31-28-47.ap-northeast-1.compute.internal>
2025-11-10 09:47:10 +0000

    update file

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,13 +5,17 @@ class BooksController < ApplicationController
     end
 
     def create
-        book = Book.new(book_params)
-        book.save
-        redirect_to book_path(book.id)
+        @book = Book.new(book_params)
+        if @book.save
+        redirect_to book_path(@book.id), notice: "Book was successfully created."
+        else
+        render :new
+      end
     end
 
     def index
         @books = Book.all
+        @book = Book.new
     end
 
     def show
@@ -23,10 +27,19 @@ class BooksController < ApplicationController
     end
 
     def update
-        book = Book.find(params[:id])
-        book.update(book_params)
-        redirect_to book_path(book.id)
+        @book = Book.find(params[:id])
+        if @book.update(book_params)
+        redirect_to book_path(@book.id), notice: "Book was successfully updated."
+        else
+            render :edit
+        end
     end
+
+    def destroy
+        @book = Book.find(params[:id])
+        @book.destroy
+        redirect_to book_path, notice: "Book was successfully destroyed."
+        end
 
         private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
 
   get "/" => "homes#top"
-  get "books/new"
-  post "books/create" => "books#create"
-  get "books/index" => "books#index"
-  get "books/:id" => "books#show", as: "book"
-  get "books/:id/edit" => "books#edit", as: "edit_book"
-  patch "books/:id" => "books#update", as: "update_book"
-  delete "books/:id" => "books#destroy", as: "destroy_book"
+  resources :books
 
 end
